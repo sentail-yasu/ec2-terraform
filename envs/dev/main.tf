9ef4a30f51aa2676d4fc5770328866673c2395b8
yasu <yasuhiro.senoo@d-ss.co.jp>
2020-05-04 21:01:20 +0900

    aaa

--- envs/dev/main.tf
+++ envs/dev/main.tf
@@ -4,7 +4,6 @@ module "dev" {
   vpc_cidr       = var.vpc_cidr
   vpc_name       = var.vpc_name
   subnet_cidr    = var.subnet_cidr
-  subnet_name    = var.subnet_name
   igw_name_tag   = var.igw_name_tag
-  
+
 }
