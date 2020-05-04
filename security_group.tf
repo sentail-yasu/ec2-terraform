c195f15e8fa5c895c06a23528b0fbd821fe58413
yasu <yasuhiro.senoo@d-ss.co.jp>
2020-05-04 22:50:48 +0900

    sg

--- security_group.tf
+++ security_group.tf
@@ -1,24 +1,6 @@
-resource "aws_security_group" "postgres" {
-    name               = "${var.name}-db-sg"
-    description        = "for postgres"
-    vpc_id             = "${aws_vpc.test.id}"
-    ingress {
-        from_port      = 5432
-        to_port        = 5432
-        protocol       = "tcp"
-        cidr_blocks    = ["10.1.0.0/16"]
-    }
-    egress {
-        from_port      = 0
-        to_port        = 0
-        protocol       = "-1"
-        cidr_blocks    = ["0.0.0.0/0"]
-    }
-}
-
 # RDSに設定するセキュリティグループ
 resource "aws_security_group" "posgre_security_group" {
-  name   = "${var.name}-db-sg"
+  name   = "${var.name}-posgre-sg"
   vpc_id = var.vpc_id
 
   ingress {
@@ -37,7 +19,7 @@ resource "aws_security_group" "posgre_security_group" {
 }
 
   tags = {
-    Name = "${var.app_name}-db-sg"
+    Name = "${var.name}-posgre-sg"
   }
 }
 
