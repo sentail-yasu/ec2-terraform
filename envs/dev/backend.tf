ee7d207e4dcd9516b9d977ddb9e42dee9cb67b6e
yasu <yasuhiro.senoo@d-ss.co.jp>
2020-05-04 20:49:14 +0900

    aa

--- envs/dev/backend.tf
+++ envs/dev/backend.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">= 0.12"
 
   backend "s3" {
-    bucket  = "tfstate-bucket-ys"
+    bucket  = "tfstate-bucket-ys-codecommit"
     region  = "ap-northeast-1"
     key     = "dev/terraform.tfstate"
     encrypt = true
