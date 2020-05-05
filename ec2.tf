9ca3d1cc5b8d897df19d1e130e148f25587decd3
yasu <yasuhiro.senoo@d-ss.co.jp>
2020-05-06 02:42:02 +0900

    aaa

--- ec2.tf
+++ ec2.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "bastion_ec2" {
   instance_type          = var.instance_type
   key_name               = var.key_pair
   subnet_id              = lookup(var.public_subnets, count.index % 2)
-  vpc_security_group_ids = [data.terraform_remote_state.security_group.outputs.bastion_sg_id]
+  vpc_security_group_ids = ["${aws_security_group.bastion_security_group.id}"]
 
   tags = {
     Name = "${var.bastion_name}_${count.index + 1}"
--- security_group.tf
+++ security_group.tf
@@ -29,7 +29,7 @@ resource "aws_security_group" "alb" {
     from_port       = "443"
     to_port         = "443"
     protocol        = "tcp"
-    cidr_blocks     = ["0.0.0.0/0"]
+    cidr_blocks     = ["118.241.89.87/32"]
   }
 
   egress {
@@ -45,4 +45,32 @@ resource "aws_security_group" "alb" {
 
 output "db_sg_id" {
   value = aws_security_group.posgre_security_group.id
+}
+
+# bastionに設定するセキュリティグループ
+resource "aws_security_group" "bastion_security_group" {
+  name   = "${var.bastion_name}-sg"
+  vpc_id = var.vpc_id
+
+  ingress {
+    from_port   = "22"
+    to_port     = "22"
+    protocol    = "tcp"
+    cidr_blocks = ["118.241.89.87/32"]
+  }
+
+  egress {
+    from_port   = "0"
+    to_port     = "0"
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.bastion_name}-sg"
+  }
+}
+
+output "bastion_sg_id" {
+  value = aws_security_group.bastion_security_group.id
 }
